7255ad28b340c628f3e6c829757168c3013426e1
mori <xinshisheng126@gmail.com>
2024-07-22 21:35:09 +0900

    add yaml parser

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,16 +1,38 @@
 import { readFileSync } from 'node:fs';
 import markdownit from 'markdown-it';
+import yaml from 'js-yaml';
+
+function mdparser(file: string){
+    let split = file.split("---\n",3);
+    let front = split[1]
+    let meta = yaml.load(front);
+    let body  = split[2]
+
+    type metadata = {
+        title: string,
+        description: string,
+        thunbnail: string,
+        category: string,
+        tags: string[]
+    }
+
+    return {
+        "metadata": meta as metadata,
+        "body": body
+    }
+    
+}
 
 export async function load({params}) {
     const postfile = readFileSync(`/Users/moris/blog/src/lib/posts/${params.slug}.md`, 'utf8');
 
-    let split_file = postfile.split("---\n",3)
-    let front_matter = split_file[1]
-    let metalist = front_matter.split("\n").filter((data)=>data!="")
-    let body_text    = split_file[2]
+    const parsed = mdparser(postfile);
+    const md = markdownit();
+    const result = md.render(parsed.body);
 
-    const md = markdownit()
-    const result = md.render(body_text);
-
-    return {"metadata": metalist, "post": result};
+    if (typeof parsed.metadata === 'object' && parsed.metadata != null){
+        return {"metadata": parsed.metadata, "post": result};
+    } else {
+        throw 'err: cannot parse front matter'
+    }
 }
