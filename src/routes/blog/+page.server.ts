3f1762b2bab6ea41993a2f4d7366fd9bfbc6f923
mori <xinshisheng126@gmail.com>
2024-07-22 14:55:07 +0900

    fix: 拡張子がダブる問題

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,6 +1,9 @@
 import {readdirSync} from 'node:fs';
+import path from 'node:path';
 
 export async function load(){
     const posts = readdirSync("/Users/moris/blog/src/lib/posts/");
-    return {"posts":posts};
+    const postname = posts.map((post)=>path.basename(post, path.extname(post)));
+
+    return {"posts":postname};
 }
