05136ea7176aa01ecd4097f0ee4466a2515ae390
mori <xinshisheng126@gmail.com>
2024-07-22 21:36:53 +0900

    change: tag style

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,10 +3,20 @@
 </script>
 
 <div class="meta">
-    <ul>
-		{#each data.metadata as meta}
-			<li>{meta}</li>
+	<div>Title: {data.metadata.title}</div>
+	<div>
+		{#each data.metadata.tags as tag}
+			<span class='tag'>{tag}</span>
 		{/each}
-	</ul>
+	</div>
 </div>
 <div>{@html data.post}</div>
+
+<style>
+	.tag {
+		background-color: darkorange;
+		border-radius: 4px;
+		padding: 2px 4px;
+		margin: 3px;
+	}
+</style>
