a76e650bfd1f3f21969b4761736fcb19077e2a5e
moris <xinshisheng126@gmail.com>
2024-08-23 21:31:28 +0900

    add: 日付でソート

--- src/lib/server/posts.ts
+++ src/lib/server/posts.ts
@@ -2,10 +2,13 @@ export default posts;
 
 import path from 'node:path';
 import { readdirSync } from 'node:fs';
+import readmd from './readmd';
 
 function posts(){
-    const postPaths = readdirSync(`${process.cwd()}/src/lib/posts`)
-    const postNames = postPaths.map((post)=>path.parse(post).name);
+    const postPaths = readdirSync(`${process.cwd()}/src/lib/posts`);
+    const postNames = postPaths.map((post)=>path.parse(post).name).sort((a,b)=>{
+        return readmd(a).metadata.date.getTime() - readmd(b).metadata.date.getTime()
+    })
 
     return postNames;
 }
