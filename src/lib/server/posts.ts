daa1e632b51bad901a724bcd7bddb1bec36cfa37
moris <xinshisheng126@gmail.com>
2024-08-25 07:58:33 +0900

    fix: 順番逆...

--- src/lib/server/posts.ts
+++ src/lib/server/posts.ts
@@ -7,7 +7,7 @@ import readmd from './readmd';
 function posts(){
     const postPaths = readdirSync(`${process.cwd()}/src/lib/posts`);
     const postNames = postPaths.map((post)=>path.parse(post).name).sort((a,b)=>{
-        return readmd(a).metadata.date.getTime() - readmd(b).metadata.date.getTime()
+        return readmd(b).metadata.date.getTime() - readmd(a).metadata.date.getTime()
     })
 
     return postNames;
