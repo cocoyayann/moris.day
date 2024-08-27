b0482f3e92c940e2a5f6a7439ad17d0d61325bfc
moris <xinshisheng126@gmail.com>
2024-08-27 21:01:44 +0900

    change: メタデータをgridで書き換えた

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -23,18 +23,19 @@
 		<h1 class="title">{data.metadata.title}</h1>
 
 		<div class="meta">
-			<div class="left">
+			<div class="category">
 				<span class="txt">Category:</span>
 				<a class="tag" href='/blog/category/{data.metadata.category}'>{data.metadata.category}</a>
-
-				{#if data.metadata.tags.length }
-					<span class="divider"></span>
-					<span class="txt">Tags:</span>
-					{#each data.metadata.tags as tag}
-						<a class='tag' href='/blog/tag/{tag}'>{tag}</a><wbr />
-					{/each}
-				{/if}
 			</div>
+
+			{#if data.metadata.tags.length }
+			<div class="tags">
+				<span class="txt">Tags:</span>
+				{#each data.metadata.tags as tag}
+					<a class='tag' href='/blog/tag/{tag}'>{tag}</a>
+				{/each}
+			</div>
+			{/if}
 			
 			<div class="date">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/></svg>
@@ -45,7 +46,8 @@
 		{#if data.metadata.thumbnail != null}
 			<img id="thumbnail" alt="thumbnail" src="/img/{data.metadata.thumbnail}">
 		{/if}
-			<Markdown mdtext={data.post} />
+
+		<Markdown mdtext={data.post} />
 	</div>
 	<aside>
 		<div id='side'>
@@ -74,7 +76,7 @@
 		}
 	
 		& #side {
-			width: 300px;
+			width: 280px;
 			display: flex;
 			flex-direction: column;
 			row-gap: 20px;
@@ -125,50 +127,55 @@
 	}
 
 	.meta {
-		display: flex;
-		position: relative;
+		display: grid;
+		grid-template-rows: auto auto;
+		grid-template-columns: auto 1fr auto;
+		gap: 8px;
 		font-size: 0.95em;
-		/*vertical-align: middle;*/
 		margin: 8px;
-		line-height: 150%;
+		white-space: nowrap;
 
 		@media(width<1000px) {
 			flex-direction: column;
 		}
 
-		& .left {
-			margin: 4px 0;
-			flex-grow: 1;
+		& .category {
+			grid-row: 1;
+			grid-column: 1;
 		}
+
+		& .tags {
+			grid-row: 1;
+			grid-column: 2;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			@media(width<1000px) {
+				grid-row: 2;
+				grid-column: 1 / 4;
+			}
+		}
+
 		& .tag {
+			display: inline-block;
 			background-color: var(--theme-color);
 			color: var(--font-color);
 			border-radius: 3px;
 			padding: 0 4px 1px 4px;
 			margin: 3px;
 		}
-		& .divider {
-			display: inline-block;
-			background-color: var(--font-color);
-			width: 1.5px;
-			height: 1.5em;
-			margin: 0 4px;
-			vertical-align: bottom;
-		}
 		& .date{
 			display: flex;
+			grid-row: 1;
+			grid-column: 3;
+			vertical-align: middle;
 			align-items: center;
-			gap: 2px;
-			margin: 4px 0;
-			font-size: 0.9em;
-			flex-shrink: 0;
 		}
 		& .date svg{
-			height: 1.2em;
+			height: 1rem;
 			fill: var(--font-color);
 			vertical-align: middle;
 		}
-		@media(width<400px) {
+		@media(width<360px) {
 			& .txt {display: none;}
 		}
 	}
